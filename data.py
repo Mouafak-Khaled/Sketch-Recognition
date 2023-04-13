6feaa84857374fc16e26680be362371006ff7d72
Mouafak Alsaid Hasan <mhasan18@ku.edu.tr>
2023-04-13 21:07:50 +0300

    Implement the QuickDrawImageDataset class

--- data.py
+++ data.py
@@ -4,91 +4,66 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset, Subset
 from torchvision.transforms.functional import adjust_contrast, adjust_sharpness
-
-
-
-class DataLoader:
-    
-    def __init__(self, config):
-        
-        self.path = config['path']
-        self.imgs, self.targets, self.labels = self.load_data()
-    
-    
-    def targets(self):
-        return self.targets
-    
-    
-    def classes(self):
-        return self.labels
-    
-    
-    def data(self):
-        return self.imgs
-    
-    
-    def load_data(self):
-    
-        imgs, targets, labels= [], [], []
-        
-        for target, file in enumerate(os.listdir(self.path)):
-            class_name = re.split("[_.]", file)[-2]
-            labels.append(class_name)
-            
-            file = os.path.join(self.path, file)
-            img_samples = np.load(file)
-            
-            imgs.extend(img_samples)
-            target_samples = [target] * img_samples.shape[0]
-            targets.extends(target_samples)
-        
-        imgs = torch.tensor(imgs)
-        targets = torch.LongTensor(targets)
-                
-        return  imgs, targets, labels
+from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor
 
 
 
 class QuickDrawDataset(Dataset):
     
-    def __init__(self, config):
+    def __init__(self, config, transforms=None):
         super(QuickDrawDataset, self).__init__()
         self.config = config
         self.path = config['path']
-        self.transforms = config['transforms']
+        self.transforms = transforms
+        
     
+    def classes(self):
+        
+        classes = []
+        for file in os.lisdir(self.path):
+            class_name = re.split("[_.]", file)[-2]
+            classes.append(class_name)
+        
+        return classes
 
+        
+    
 class QucikDrawImageDataset(QuickDrawDataset):
     
-    def __init__(self, loader, config):
-        super(QucikDrawImageDataset, self).__init__(config)
-        self.loader = loader
-        self.img_shape = 
+    def __init__(self, config, transforms=None):
+        super(QucikDrawImageDataset, self).__init__(config, transforms=transforms)
+        self.img_shape = config['img_shape']
+        self.img_files = os.listdir(self.path)
+        
         
     def __len__(self):
-        return len(self.loader.targets())
-    
+        return sum([np.load(os.path.join(self.path, file)).
+                    shape[0] for file in self.img_files])
+
     
     def __getitem__(self, index):
         
-        img = self.loader.data()[index].reshape(self.img_shape)
-        target = self.loader.targets()[index]
+        if index < 0:
+            index += self.__len__()
         
-        if self.transforms:
-            img = self.transforms(img)
+        for target, file in enumerate(self.img_files):
+            imgs = np.load(os.path.join(self.path, file))
+            if index < len(imgs):
+                img = imgs[index].reshape(self.img_shape)
+                
+                if self.transforms:
+                    img = self.transforms(img)
+                    
+                img = adjust_contrast(img, contrast_factor=2)
+                img = adjust_sharpness(img, sharpness_factor=2)
+                
+                return img, target
+            
+            index -= len(imgs)
         
-        img = adjust_contrast(img, contrast_factor=2)
-        img = adjust_sharpness(img, sharpness_factor=2)
         
         
-        return img, target
-        
-
-
-
-
-
-
 class QuickDrawStrokeDataset(QuickDrawDataset):
     
     def __init__(self,
